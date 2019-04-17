cacc05ac07b9f772021cb41436fb24d31f6fef4e
Nguyen Hong Hai <nguyen.hong.hai@sun-asterisk.com>
2019-04-17 23:53:35 +0700

    Vue CLI

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Home</div>
+</template>
+
+<script>
+  export default {
+    created() {
+      axios.get('users')
+        .then((response) => {
+          console.log(response);
+        });
+    }
+  }
+</script>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
